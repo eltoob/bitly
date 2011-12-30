e373e6918d14714e95c63c96965cff5f9aa90bd8
Elie Toubiana <elie.toubiana@gmail.com>
2011-12-30 12:46:01 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bitly::Application.config.secret_token = '209153d67082438601341080843615833a81d27243aaa90d4fe916d83c525a2d75fcbdfda63823e99b59ce35ef2a858ac2f537efa4d8e93c6efce33f03cf98cd'
